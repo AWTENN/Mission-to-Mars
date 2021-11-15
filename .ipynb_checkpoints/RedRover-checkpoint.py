fca8e7e2ed708188a63e852891ea7a667036d102
AWTENN <89875730+AWTENN@users.noreply.github.com>
2021-11-15 09:56:57 -0600

    App

---
+++ .ipynb_checkpoints/RedRover-checkpoint.py
@@ -0,0 +1,375 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "a13cd20f-f2d5-42aa-92f4-5b8bd128a3f5",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Import Splinter and BeautifulSoup\n",
+    "from splinter import Browser\n",
+    "from bs4 import BeautifulSoup as soup\n",
+    "from webdriver_manager.chrome import ChromeDriverManager"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "dad7e94c-303f-4f52-be0f-c3a50df4db87",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "\n",
+      "\n",
+      "====== WebDriver manager ======\n",
+      "Current google-chrome version is 95.0.4638\n",
+      "Get LATEST chromedriver version for 95.0.4638 google-chrome\n",
+      "Driver [C:\\Users\\CFP76\\.wdm\\drivers\\chromedriver\\win32\\95.0.4638.69\\chromedriver.exe] found in cache\n"
+     ]
+    }
+   ],
+   "source": [
+    "executable_path = {'executable_path': ChromeDriverManager().install()}\n",
+    "browser = Browser('chrome', **executable_path, headless=False)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "512a13fd-9840-4d9a-bdc0-978772bbb78c",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "True"
+      ]
+     },
+     "execution_count": 3,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# Visit the mars nasa news site\n",
+    "url = 'https://redplanetscience.com'\n",
+    "browser.visit(url)\n",
+    "# Optional delay for loading the page\n",
+    "browser.is_element_present_by_css('div.list_text', wait_time=1)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "a35dde23-e5ec-4bbc-b7ed-33d31cc0f623",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "html = browser.html\n",
+    "news_soup = soup(html, 'html.parser')\n",
+    "slide_elem = news_soup.select_one('div.list_text')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "dc06599a-b1d4-47f0-ae13-6195fa5c026c",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "<div class=\"content_title\">Two Rovers to Roll on Mars Again: Curiosity and Mars 2020</div>"
+      ]
+     },
+     "execution_count": 5,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "slide_elem.find('div', class_='content_title')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "7b6c7439-ffab-4731-9a65-7b418436b5a6",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'Two Rovers to Roll on Mars Again: Curiosity and Mars 2020'"
+      ]
+     },
+     "execution_count": 6,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# Use the parent element to find the first `a` tag and save it as `news_title`\n",
+    "news_title = slide_elem.find('div', class_='content_title').get_text()\n",
+    "news_title"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "e9b82be6-59d6-4d4e-b1cb-9c8297abbc87",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'They look like twins. But under the hood, the rover currently exploring the Red Planet and the one launching there this summer have distinct science tools and roles to play.'"
+      ]
+     },
+     "execution_count": 7,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# Use the parent element to find the paragraph text\n",
+    "news_p = slide_elem.find('div', class_='article_teaser_body').get_text()\n",
+    "news_p"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "cefe02eb-338e-4950-82f7-79a0d63b5d0b",
+   "metadata": {},
+   "source": [
+    "### Featured Images"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "cd36727b-e2e0-4855-aea7-a9841d3f33d0",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Visit URL\n",
+    "url = 'https://spaceimages-mars.com'\n",
+    "browser.visit(url)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "82c2b4a7-4b03-4b66-a796-ee396fde68d8",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Find and click the full image button\n",
+    "full_image_elem = browser.find_by_tag('button')[1]\n",
+    "full_image_elem.click()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "id": "8c739b10-4ebf-4cde-881f-b8f74156d1aa",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Parse the resulting html with soup\n",
+    "html = browser.html\n",
+    "img_soup = soup(html, 'html.parser')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "id": "e3b7bf42-55ce-4388-a3f2-d441eb61ff30",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'image/featured/mars1.jpg'"
+      ]
+     },
+     "execution_count": 11,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# Find the relative image url\n",
+    "img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')\n",
+    "img_url_rel"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 12,
+   "id": "deba5467-d9ba-4fd1-9978-d9042bb90610",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'https://spaceimages-mars.com/image/featured/mars1.jpg'"
+      ]
+     },
+     "execution_count": 12,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# Use the base URL to create an absolute URL\n",
+    "img_url = f'https://spaceimages-mars.com/{img_url_rel}'\n",
+    "img_url"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 14,
+   "id": "5c06f525-3f33-4e5d-8f11-212e98b74003",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/html": [
+       "<div>\n",
+       "<style scoped>\n",
+       "    .dataframe tbody tr th:only-of-type {\n",
+       "        vertical-align: middle;\n",
+       "    }\n",
+       "\n",
+       "    .dataframe tbody tr th {\n",
+       "        vertical-align: top;\n",
+       "    }\n",
+       "\n",
+       "    .dataframe thead th {\n",
+       "        text-align: right;\n",
+       "    }\n",
+       "</style>\n",
+       "<table border=\"1\" class=\"dataframe\">\n",
+       "  <thead>\n",
+       "    <tr style=\"text-align: right;\">\n",
+       "      <th></th>\n",
+       "      <th>Mars</th>\n",
+       "      <th>Earth</th>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>description</th>\n",
+       "      <th></th>\n",
+       "      <th></th>\n",
+       "    </tr>\n",
+       "  </thead>\n",
+       "  <tbody>\n",
+       "    <tr>\n",
+       "      <th>Mars - Earth Comparison</th>\n",
+       "      <td>Mars</td>\n",
+       "      <td>Earth</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Diameter:</th>\n",
+       "      <td>6,779 km</td>\n",
+       "      <td>12,742 km</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Mass:</th>\n",
+       "      <td>6.39 × 10^23 kg</td>\n",
+       "      <td>5.97 × 10^24 kg</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Moons:</th>\n",
+       "      <td>2</td>\n",
+       "      <td>1</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Distance from Sun:</th>\n",
+       "      <td>227,943,824 km</td>\n",
+       "      <td>149,598,262 km</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Length of Year:</th>\n",
+       "      <td>687 Earth days</td>\n",
+       "      <td>365.24 days</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Temperature:</th>\n",
+       "      <td>-87 to -5 °C</td>\n",
+       "      <td>-88 to 58°C</td>\n",
+       "    </tr>\n",
+       "  </tbody>\n",
+       "</table>\n",
+       "</div>"
+      ],
+      "text/plain": [
+       "                                    Mars            Earth\n",
+       "description                                              \n",
+       "Mars - Earth Comparison             Mars            Earth\n",
+       "Diameter:                       6,779 km        12,742 km\n",
+       "Mass:                    6.39 × 10^23 kg  5.97 × 10^24 kg\n",
+       "Moons:                                 2                1\n",
+       "Distance from Sun:        227,943,824 km   149,598,262 km\n",
+       "Length of Year:           687 Earth days      365.24 days\n",
+       "Temperature:                -87 to -5 °C      -88 to 58°C"
+      ]
+     },
+     "execution_count": 14,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "import pandas as pd\n",
+    "df = pd.read_html('https://galaxyfacts-mars.com')[0]\n",
+    "df.columns=['description', 'Mars', 'Earth']\n",
+    "df.set_index('description', inplace=True)\n",
+    "df"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 15,
+   "id": "bf7c822b-b351-4aab-8ae9-1092c6066049",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "browser.quit()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8e48c6a5-a65c-4a5f-bc3c-392f5d8dd9b2",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "PythonData",
+   "language": "python",
+   "name": "pythondata"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.10"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
