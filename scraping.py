08aae2af564d5c9e796648155703c22db46f8ceb
AWTENN <89875730+AWTENN@users.noreply.github.com>
2021-11-21 23:19:05 -0600

    Final Landing

--- scraping.py
+++ scraping.py
@@ -1,7 +1,7 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 
-from splinter import Browser
+from splinter import Browser, browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
 import datetime as dt
@@ -15,14 +15,15 @@ def scrape_all():
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
-
+    hemi_imgs_titles = hemispheres(browser)
     # Run all scraping functions and store results in dictionary
     data = {
       "news_title": news_title,
       "news_paragraph": news_paragraph,
       "featured_image": featured_image(browser),
       "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
+      "last_modified": dt.datetime.now(),
+      "hemispheres" : hemi_imgs_titles
     }
   # Stop webdriver and return data
     browser.quit()
@@ -104,6 +105,34 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
 
+def hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url1 = 'https://marshemispheres.com/'
+    browser.visit(url1)
+
+
+# %%
+# 2. Create a list to hold the images and titles.
+    hemi_imgs_titles = []
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    links = browser.find_by_css('a.product-item img')
+    for i in range(len(links)):
+        hemispheres = {}
+        browser.find_by_css('a.product-item img')[i].click()
+        hemispheres = {}
+
+        iur = browser.links.find_by_text('Sample').first
+        iurl = iur['href']
+        title = browser.find_by_css('h2.title').text
+        hemispheres['img_url'] = iurl
+        hemispheres['title'] = title
+        hemi_imgs_titles.append(hemispheres)
+        browser.back()
+
+
+    # %%
+    # 4. Print the list that holds the dictionary of each image url and title.
+    return hemi_imgs_titles
 if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
